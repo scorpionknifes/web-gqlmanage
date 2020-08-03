da21a4c19acb51ec21aee9a0ac1f3aa623a3bd82
Cheng-Zhen Yang <chengzhenyang@gmail.com>
2020-08-04 04:06:25 +1200

    add favicon and theme

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import { AddDevice, EditDevice, Devices, Device } from './pages/Device'
 import { User, AddUser, Users, EditUser } from './pages/User';
 import Emails from './pages/Email/Emails';
 import { SubscriptionClient } from 'subscriptions-transport-ws'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core'
 
 const httpLink = createHttpLink({
     uri: process.env.REACT_APP_GRAPHQL_URL,
@@ -29,7 +30,7 @@ const subscriptionClient = new SubscriptionClient(
         reconnect: true,
     },
     null,
-    [ "graphql-ws", localStorage.getItem('token') ])
+    ["graphql-ws", localStorage.getItem('token')])
 
 const wsLink = new WebSocketLink(subscriptionClient);
 
@@ -62,37 +63,50 @@ const client = new ApolloClient({
     cache: new InMemoryCache()
 });
 
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#c30000',
+      },
+      secondary: {
+        main: '#c30000',
+      },
+    },
+  });
+
 function App() {
     return (
-        <BrowserRouter basename={'/manage'}>
-            <ApolloProvider client={client}>
-                <MenuAppBar>
-                    <Switch>
-                        <Route exact path="/login" component={Login} />
+        <ThemeProvider theme={theme}>
+            <BrowserRouter basename={'/manage'}>
+                <ApolloProvider client={client}>
+                    <MenuAppBar>
+                        <Switch>
+                            <Route exact path="/login" component={Login} />
 
-                        <Route exact path="/rooms" component={Rooms} />
-                        <Route exact path="/room/add" component={AddRoom} />
-                        <Route exact path="/room/:id" component={Room} />
-                        <Route exact path="/room/edit/:id" component={EditRoom} />
+                            <Route exact path="/rooms" component={Rooms} />
+                            <Route exact path="/room/add" component={AddRoom} />
+                            <Route exact path="/room/:id" component={Room} />
+                            <Route exact path="/room/edit/:id" component={EditRoom} />
 
-                        <Route exact path="/devices" component={Devices} />
-                        <Route exact path="/devices/:id" component={Devices} />
-                        <Route exact path="/device/add/:id" component={AddDevice} />
-                        <Route exact path="/device/:id" component={Device} />
-                        <Route exact path="/device/edit/:id" component={EditDevice} />
+                            <Route exact path="/devices" component={Devices} />
+                            <Route exact path="/devices/:id" component={Devices} />
+                            <Route exact path="/device/add/:id" component={AddDevice} />
+                            <Route exact path="/device/:id" component={Device} />
+                            <Route exact path="/device/edit/:id" component={EditDevice} />
 
-                        <Route exact path="/users" component={Users} />
-                        <Route exact path="/user/:id" component={User} />
-                        <Route exact path="/user/add" component={AddUser} />
-                        <Route exact path="/user/edit/:id" component={EditUser} />
+                            <Route exact path="/users" component={Users} />
+                            <Route exact path="/user/:id" component={User} />
+                            <Route exact path="/user/add" component={AddUser} />
+                            <Route exact path="/user/edit/:id" component={EditUser} />
 
-                        <Route exact path="/emails" component={Emails} />
+                            <Route exact path="/emails" component={Emails} />
 
-                        <Redirect to="/rooms" />
-                    </Switch>
-                </MenuAppBar>
-            </ApolloProvider>
-        </BrowserRouter>
+                            <Redirect to="/rooms" />
+                        </Switch>
+                    </MenuAppBar>
+                </ApolloProvider>
+            </BrowserRouter>
+        </ThemeProvider>
     );
 }
 
